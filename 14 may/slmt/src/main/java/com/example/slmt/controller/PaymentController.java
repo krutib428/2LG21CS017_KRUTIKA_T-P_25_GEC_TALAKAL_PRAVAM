3fc7a69bd9e12a7124cfb306bdb7255b08ead774
krutika <krutikav219@gmail.com>
2025-05-17 12:25:27 +0530

    new changes with bootstrap lib

--- 14 may/slmt/src/main/java/com/example/slmt/controller/PaymentController.java
+++ 14 may/slmt/src/main/java/com/example/slmt/controller/PaymentController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import com.example.slmt.model.Payment;
 import com.example.slmt.service.PaymentService;
 
